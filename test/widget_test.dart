368ecc96e3b2d513b01282e98e865438c5140efb
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2026-03-02 09:35:21 +0100

    For some weird reason, the test was expected to find no widgets

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,6 +6,7 @@ import 'package:refix_website/main.dart';
 void main() {
   testWidgets('App smoke test', (WidgetTester tester) async {
     await tester.pumpWidget(const ProviderScope(child: RefixApp()));
-    expect(find.byType(MaterialApp), findsNothing); // uses MaterialApp.router
+    expect(find.byType(MaterialApp), findsOneWidget);
+    expect(find.text('View Color Palette'), findsOneWidget);
   });
 }
